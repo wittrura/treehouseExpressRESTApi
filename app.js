1e400a6ab292296880a4124b6453056deac397be
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-03-05 01:04:21 -0500

    Updates routes to get data from mongo

--- app.js
+++ app.js
@@ -9,8 +9,23 @@ var logger = require('morgan');
 const PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
-
 app.use(jsonParser());
+
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/qa');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+  console.error("connection error: ", err);
+});
+
+db.once('open', function(){
+  console.log("connected to MongoDb");
+});
+
 
 app.use("/questions", routes);
 
--- routes.js
+++ routes.js
@@ -2,66 +2,106 @@
 
 var express = require('express');
 var router = express.Router();
+var Question = require('./models').Question;
 
-
-router.get("/", function(req, res){
-  res.json({response: "received GET request at /questions"});
-});
-
-router.post("/", function(req, res){
-  res.json({
-    response: "received POST request at /questions",
-    body: req.body
+router.param("qid", function(req, res, next, id){
+  Question.findById(req.params.qid, function(err, doc){
+    if (err) return next(err);
+    if (!doc) {
+      err = new Error("Not Found");
+      err.status = 404;
+      return next(err);
+    }
+    req.question = doc;
+    return next();
   });
 });
 
-router.get("/:qid", function(req, res){
-  res.json({
-    response: "received GET request at /questions for ID " + req.params.qid
+router.param("aid", function(req, res, next, id){
+  req.answer = req.question.answers.id(id);
+  if (!req.answer){
+    err = new Error("Not Found");
+    err.status = 404;
+    return next(err);
+  }
+  next();
+});
+
+// GET /questions
+// return all questions
+router.get("/", function(req, res, next){
+  Question.find({})
+          .sort({createdAt: -1})
+          .exec(function(err, questions){
+            if (err) return next(err);
+            res.json(questions);
+          });
+});
+
+// POST /questions
+// create new question document
+router.post("/", function(req, res, next){
+  var question = new Question(req.body);
+  question.save(function(err, question){
+    if (err) return next(err);
+    res.status(201);
+    res.json(question);
   });
 });
 
-router.post("/:qid/answers", function(req, res){
-  res.json({
-    response: "recevied POST request to /answers",
-    questionId: req.params.qid,
-    body: req.body
+// GET /questions/####
+// get a question
+router.get("/:qid", function(req, res, next){
+  res.json(req.question);
+});
+
+// POST /questions/####/answers
+// post answer to a question
+router.post("/:qid/answers", function(req, res, next){
+  req.question.answers.push(req.body);
+  req.question.save(function(err, question){
+    if (err) return next(err);
+    res.status(201);
+    res.json(question);
   });
 });
 
+// PUT /questions/####/aswers/####
+// update answer to a question
 router.put("/:qid/answers/:aid", function(req, res){
-  res.json({
-    response: "recevied PUT request to /answers",
-    questionId: req.params.qid,
-    answerId: req.params.aid,
-    body: req.body
+  req.answer.update(req.body, function(err, answer){
+    if (err) return next(err);
+    res.json(answer);
   });
 });
 
+// DELETE /questions/####/aswers/####
+// delete answer to a question
 router.delete("/:qid/answers/:aid", function(req, res){
-  res.json({
-    response: "recevied DELETE request to /answers",
-    questionId: req.params.qid,
-    answerId: req.params.aid,
-    body: req.body
+  req.answer.remove(function(err){
+    req.question.save(function(err, question){
+      if (err) return next(err);
+      res.json(question);
+    });
   });
 });
 
+// POST /questions/####/answers/####/vote-xxxx
+// post to an answer
 router.post("/:qid/answers/:aid/vote-:dir", function(req, res, next){
   if(req.params.dir.search(/^(up|down)$/) === -1){
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
   } else {
+    req.vote = req.params.dir;
+
     next();
   }
-}, function(req, res){
-  res.json({
-    response: "recevied POST request to /answers for /vote-" + req.params.dir,
-    questionId: req.params.qid,
-    answerId: req.params.aid,
-    voteDirection: req.params.dir,
-    body: req.body
+}, function(req, res, next){
+  req.answer.vote(req.vote, function(err, question){
+    if (err) return next(err);
+    res.json(question);
   });
 });
 
